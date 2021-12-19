4f42e5255d1e45ef7b0436666bfce20b87260e8e
Patricia <anapatriciamantamoreira@gmail.com>
2021-12-19 19:09:32 +0000

    Header bem ainda falta Footer

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,4 +6,43 @@ Laravel Programming Lab. | About Page
 
 @section('content')
 <h2 class="mt-5 mb-3 text-center">About!</h2>
+<p>Maecenas faucibus, nunc eget hendrerit ultrices, nisi turpis vehicula sapien, sed pulvinar lacus felis eu felis. Interdum et malesuada fames ac ante ipsum primis in 
+    faucibus. Maecenas imperdiet lacus mi, vitae ullamcorper sem sollicitudin vel. Suspendisse nec eros libero. Phasellus nunc tellus, aliquet quis dictum in, lacinia ac 
+    leo. Etiam eu malesuada tortor. Morbi porttitor ullamcorper nisi quis lobortis. Mauris pharetra dui id purus varius, at finibus nunc consectetur. Vivamus fermentum 
+    arcu vitae sagittis dictum. Maecenas pellentesque aliquam ante. Suspendisse a dictum lectus, accumsan gravida ex.</p>
+
+<p>Mauris elementum odio massa, sit amet porttitor ipsum mattis at. Donec at semper neque, eu volutpat metus. Nulla egestas nisl vel justo facilisis, vulputate viverra leo 
+    ornare. Donec et faucibus massa, id ullamcorper sapien. Mauris volutpat at eros vel rutrum. Praesent vulputate, tortor a varius pellentesque, felis sem finibus neque, 
+    ut lacinia felis magna vitae dolor. Donec ut aliquam orci, ac aliquet felis. Duis facilisis velit vitae pellentesque consectetur.</p>
+
+<p>Aenean a iaculis urna, a maximus magna. Vivamus placerat diam et tincidunt vehicula. Praesent pellentesque tempor elit, quis iaculis sem aliquet quis. Nam ligula leo, 
+    pretium ac porta sit amet, rutrum at est. Etiam pharetra nulla pretium, elementum sapien lobortis, dapibus diam. Fusce et euismod dolor. Vestibulum lobortis massa 
+    magna, in porttitor enim tempus at. Vivamus et hendrerit nisl. Suspendisse tellus neque, gravida bibendum tincidunt in, fringilla nec tortor. Praesent quis convallis 
+    orci.</p>
+
+<p>Duis elementum rhoncus ligula. Duis at nulla ipsum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque volutpat 
+    augue in eros tincidunt, quis sodales tellus sodales. Integer tempus, dolor a viverra pulvinar, justo lorem rutrum eros, id pretium diam lectus convallis augue. 
+    Aenean sed viverra est. Nulla tincidunt posuere erat, a iaculis dolor tempus eu. Proin justo elit, tincidunt efficitur cursus ut, aliquam id ipsum. Nulla fringilla 
+    metus mollis eros consectetur lacinia. Proin sollicitudin, ipsum ac sodales iaculis, dui tortor molestie velit, eget auctor nibh tellus sit amet velit.</p>
+
+<p>Nam sed ipsum at purus faucibus interdum. Mauris vehicula eget nulla ut rhoncus. Duis lobortis a dolor nec pretium. Curabitur gravida eleifend mattis. Quisque elit 
+    purus, tempus non sem non, varius tempus lacus. Aliquam et elementum dolor, vel ullamcorper tellus. Phasellus diam libero, elementum at tellus vitae, faucibus 
+    faucibus eros. Fusce eu tincidunt eros. In hac habitasse platea dictumst. Suspendisse lacinia porttitor vestibulum.</p>
+
+<p>Aliquam quis sem feugiat, fermentum ipsum eget, ultricies arcu. Vestibulum quis dignissim lorem. Sed sit amet auctor eros, nec dictum nisi. Aenean ultricies, 
+    nisl non condimentum ultricies, velit felis mollis ligula, sed ornare ipsum ante vel diam. In et vestibulum urna. Pellentesque auctor imperdiet commodo. In 
+    quis turpis dictum, sodales risus sed, dictum nisl. Sed vel tincidunt massa. Aliquam erat volutpat.</p>
+
+<p>Morbi in nulla ac purus commodo facilisis. Cras elementum mattis vulputate. Sed et vehicula lectus. Nullam rhoncus volutpat interdum. Aenean nec turpis ex. 
+    Maecenas pharetra lacus justo, sed tempus orci molestie sollicitudin. In convallis tristique luctus. Nullam non leo odio. Nulla interdum sollicitudin 
+    lectus non sagittis.</p>
+
+<p>Duis elementum rhoncus ligula. Duis at nulla ipsum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque volutpat 
+    augue in eros tincidunt, quis sodales tellus sodales. Integer tempus, dolor a viverra pulvinar, justo lorem rutrum eros, id pretium diam lectus convallis augue. 
+    Aenean sed viverra est. Nulla tincidunt posuere erat, a iaculis dolor tempus eu. Proin justo elit, tincidunt efficitur cursus ut, aliquam id ipsum. Nulla fringilla 
+    metus mollis eros consectetur lacinia. Proin sollicitudin, ipsum ac sodales iaculis, dui tortor molestie velit, eget auctor nibh tellus sit amet velit.</p>    
+
+<p>Morbi in nulla ac purus commodo facilisis. Cras elementum mattis vulputate. Sed et vehicula lectus. Nullam rhoncus volutpat interdum. Aenean nec turpis ex. 
+    Maecenas pharetra lacus justo, sed tempus orci molestie sollicitudin. In convallis tristique luctus. Nullam non leo odio. Nulla interdum sollicitudin 
+    lectus non sagittis.</p>    
 @endsection
